c2f7fd5c4064b45630ec2a4ab706b4079510438e
smartinezai <samescobar.ai@gmail.com>
2026-04-01 17:55:09 +0200

    added log tools in a separate file

---
+++ app/logs.py
@@ -0,0 +1,48 @@
+import os
+import json
+import secrets
+from pathlib import Path
+from datetime import datetime
+from pydantic_ai.messages import ModelMessagesTypeAdapter
+
+
+LOG_DIR = Path(os.getenv('LOGS_DIRECTORY', 'logs'))
+LOG_DIR.mkdir(exist_ok=True)
+
+
+def log_entry(agent, messages, source="user"):
+    tools = []
+    for ts in agent.toolsets:
+        tools.extend(ts.tools.keys())
+    dict_messages = ModelMessagesTypeAdapter.dump_python(messages)
+    return {
+        "agent_name": agent.name,
+        "system_prompt": agent._instructions,
+        "provider": agent.model.system,
+        "model": agent.model.model_name,
+        "tools": tools,
+        "messages": dict_messages,
+        "source": source
+    }
+
+
+def serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+def log_interaction_to_file(agent, messages, source='user'):
+    entry = log_entry(agent, messages, source)
+    ts = entry['messages'][-1]['timestamp']
+    if isinstance(ts, str):
+        ts_obj = datetime.fromisoformat(ts.replace("Z", "+00:00"))
+    else:
+        ts_obj = ts
+    ts_str = ts_obj.strftime("%Y%m%d_%H%M%S")
+    rand_hex = secrets.token_hex(3)
+    filename = f"{agent.name}_{ts_str}_{rand_hex}.json"
+    filepath = LOG_DIR / filename
+    with filepath.open("w", encoding="utf-8") as f_out:
+        json.dump(entry, f_out, indent=2, default=serializer)
+    return filepath
